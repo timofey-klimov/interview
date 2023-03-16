ce5b0970cb82a706c7b53584c69afa099d2f4836
timofey-klimov <timofey.klimov@inbox.ru>
2023-03-16 13:11:35 +0300

    commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿A a = new A();
+a.Method();
+
+A b = new B();
+b.Method();
+
+A c = new C();
+c.Method();
+
+C cc = new C();
+cc.Method();
+
+
+
+class A
+{
+    public virtual void Method()
+    {
+        Console.WriteLine("From A");
+    }
+}
+
+
+class B : A
+{
+    public override void Method()
+    {
+        Console.WriteLine("From B");
+    }
+}
+
+class C : B
+{
+    public new void Method()
+    {
+        Console.WriteLine("From C");
+    }
+}
+
